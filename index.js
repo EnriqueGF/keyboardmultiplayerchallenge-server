da8f75ad043446df9b521c1980e17c1cd9e3335c
Enrique <enrik.garcia98@gmail.com>
2022-02-22 23:35:31 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
